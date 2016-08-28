38ca109a85e62d9407654ab13d96cca13220073d
Alex Belenky <alex.b@matomy.com>
2016-08-28 15:50:52 +0300

    init

---
+++ lib/aerospike/rails.rb
@@ -0,0 +1,8 @@
+require "aerospike/rails/version"
+require "active_support/cache/aerospike_store"
+
+module Aerospike
+  module Rails
+    # Your code goes here...
+  end
+end
---
+++ lib/aerospike/rails/version.rb
@@ -0,0 +1,5 @@
+module Aerospike
+  module Rails
+    VERSION = "0.1.0"
+  end
+end
